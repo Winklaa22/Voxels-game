419f6627c0ec6e49e352c7426dae60ccfac23737
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2024-08-20 15:15:41 +0200

    bug fixing

--- Voxels game/Assets/Scripts/Controllers/Block Collection/BlocksGalleryController.cs
+++ Voxels game/Assets/Scripts/Controllers/Block Collection/BlocksGalleryController.cs
@@ -1,14 +1,25 @@
 using Assets.Scripts.Management.Blocks_Collection;
+using Assets.Scripts.UI.Gallery;
 using Controllers.Pause;
+using Inventory;
 using Management.UI;
+using Management.VoxelManagement;
 using UnityEngine;
 
 namespace Assets.Scripts.Controllers.Block_Collection
 {
     public class BlocksGalleryController : MonoBehaviour
     {
+        [SerializeField] private GallerySlot[] _slots;
+
         private void Start()
         {
+            for (int i = 0; i < _slots.Length; i++)
+            {
+                GallerySlot slot = _slots[i];
+                slot.ChangeBlockPicture(VoxelProperties.GetBlockByType(InventoryManager.Instance.Slots[i]));
+            }
+
             foreach (var item in BlocksGalleryManager.Instance.Items)
             {
                 UIManager.Instance.AddGallerySlot(item);
